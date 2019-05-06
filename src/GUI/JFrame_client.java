8ea189d4ff5c9d004688e8a296649a2589993d5a
Mohammad747 <mohammad.hashemi747@gamil.com>
2019-05-06 15:48:15 +0430

    Fix showing the names and add a Title

--- src/GUI/JFrame_client.java
+++ src/GUI/JFrame_client.java
@@ -154,11 +154,9 @@ public class JFrame_client extends javax.swing.JFrame {
             String msgin = "";
             while (!msgin.equals("exit")){
                 msgin = din.readUTF();
-                jTextArea_client.setText(jTextArea_client.getText().trim() +
-                        "\n" + msgin);
+                jTextArea_client.setText(jTextArea_client.getText().trim() + "\n" + msgin);
 
             }
-
 
         }catch (Exception e){
             e.getMessage();
